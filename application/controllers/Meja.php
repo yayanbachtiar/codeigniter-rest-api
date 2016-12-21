d9e5cc1ab240fef8bfe31e253cced1e28d1e71e2
bachtiaryayan <yayan.bachtiar@ralali.com>
2016-12-21 23:43:03 +0700

    add rest helper

---
+++ application/controllers/Meja.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Meja extends REST_Controller {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index_get() {
+        echo 'gembus';
+    }
+}

7a2619c8b0ec2d93f90df97c44614babce2ad499
yayanbachtiar <abiedoank@gmail.com>
2016-12-22 22:57:55 +0700

    change git ignore

--- application/controllers/Meja.php
+++ application/controllers/Meja.php
@@ -3,11 +3,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 
 class Meja extends REST_Controller {
-    public function __construct() {
-        parent::__construct();
-    }
-
-    public function index_get() {
-        $this->response('gembus');
-    }
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('Meja_model');
+		
+	}
+	
+	public function index_get() {
+		$data = $this->Meja_model->get_all_meja();
+		
+		$this->response($data, REST_Controller::HTTP_OK);
+	}
+	
+	function index_post()
+				    {
+		$this->load->library('form_validation');
+		
+		$this->form_validation->set_rules('Nomor','Nomor','is_unique[meja.Nomor]');
+		$this->form_validation->set_rules('Posisi','Posisi','max_length[100]');
+		
+		if($this->form_validation->run())     
+								        {
+			$params = array(
+                'Nomor' => $this->input->post('Nomor'),
+                'Posisi' => $this->input->post('Posisi'),
+                'created_at' => $this->input->post('created_at'),
+            );
+			
+			$this->Meja_model->add_meja($params);
+			$message = ['message'=> "created", "code"=> true];            
+			$this->set_response($message, REST_Controller::HTTP_CREATED);
+		}
+		else{
+			$message = ['message'=> validation_errors(), 'code'=> false];
+			$this->set_response($message, REST_Controller::HTTP_CREATED);
+		}
+	}
+		
+	function index_delete($id)
+    {
+        // $id = (int) $this->get('id');
+		$meja = $this->Meja_model->get_meja($id);
+		
+		// 		check if the meja exists before trying to delete it
+        $message = ["message"=>"data is not exist", "code" => false]; 
+        if(isset($meja['id']))
+        {
+			$this->Meja_model->delete_meja($id);
+            $message = ['message'=>'successfull deleted', "code" => true];
+			$this->set_response($message, REST_Controller::HTTP_OK);
+        }else{
+			$this->set_response($message, REST_Controller::HTTP_NOT_FOUND);            
+        }
+        
+	}
+	
 }

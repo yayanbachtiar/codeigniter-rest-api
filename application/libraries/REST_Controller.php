889a86d0fdfe6ee33ee0bb20c58c178be0c8beb7
yayanbachtiar <abiedoank@gmail.com>
2016-12-24 23:36:16 +0700

    access control origin *

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -378,6 +378,13 @@ abstract class REST_Controller extends CI_Controller {
      */
     public function __construct($config = 'rest')
     {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == "OPTIONS") {
+            exit();
+        }
         parent::__construct();
 
         $this->preflight_checks();
